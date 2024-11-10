6e9be17c860fe7da0c84598b09e1c53e26c86b7d
Jorge Roldan <jorge.roldan@gmail.com>
2024-11-10 17:58:47 +0100

    Limit ship movement and play in fullscreen

--- src/ship.py
+++ src/ship.py
@@ -21,9 +21,9 @@ class Ship:
 
   def update(self):
     """Updates ship position"""
-    if self.moving_right:
+    if self.moving_right and self.rect.right < self.screen_rect.right:
       self.x += self.settings.ship_speed
-    elif self.moving_left:
+    elif self.moving_left and self.rect.left > 0:
       self.x -= self.settings.ship_speed
 
     self.rect.x = self.x
