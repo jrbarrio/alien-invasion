5f6b747ab28abd5eac64870e05ea0cb83b240781
Jorge Roldan <jorge.roldan@gmail.com>
2024-11-11 19:24:26 +0100

    Detect collisions

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -81,6 +81,15 @@ class AlienInvasion:
       if bullet.rect.bottom <= 0:
         self.bullets.remove(bullet)
 
+    self._check_bullet_alien_collisions()
+
+  def _check_bullet_alien_collisions(self): 
+    collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+    if not self.aliens:
+      self.bullets.empty()
+      self._create_fleet()
+
   def _create_fleet(self):
     """Create the fleet of aliens."""
     # Create an alien and keep adding aliens until there's no room left.
