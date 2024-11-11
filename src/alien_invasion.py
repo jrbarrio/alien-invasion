85e50c544d6c6e7037ea57d7932858c629300f6f
Jorge Roldan <jorge.roldan@gmail.com>
2024-11-11 18:59:23 +0100

    Create alien rows

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -81,8 +81,28 @@ class AlienInvasion:
         self.bullets.remove(bullet)
 
   def _create_fleet(self):
+    """Create the fleet of aliens."""
+    # Create an alien and keep adding aliens until there's no room left.
+    # Spacing between aliens is one alien width and one alien height.
     alien = Alien(self)
-    self.aliens.add(alien)
+    alien_width, alien_height = alien.rect.size
+
+    current_x, current_y = alien_width, alien_height
+    while current_y < (self.settings.screen_height - 10 * alien_height):
+      while current_x < (self.settings.screen_width - 2 * alien_width):
+        self._create_alien(current_x, current_y)
+        current_x += 2 * alien_width
+
+      # Finished a row; reset x value, and increment y value.
+      current_x = alien_width
+      current_y += 2 * alien_height
+  
+  def _create_alien(self, x_position, y_position):
+    new_alien = Alien(self)
+    new_alien.x = x_position
+    new_alien.rect.x = x_position
+    new_alien.rect.y = y_position
+    self.aliens.add(new_alien)
 
   def _update_screen(self):
     """Updates images and refreshes the screen"""
